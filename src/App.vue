03c8e67893ce842040e92617f5e26d83cd800043
Cheongham <hblee66@nate.com>
2022-01-09 23:53:40 +0900

    vue study hard 3!!!

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
+# 뷰 설치방법
+#     1. nodejs 최신버전을 설치 한다
+#     2. vscode 설치
+#     3. 터미널에서 npm install -g @vue/cli 를 입력 (vue cli설치함)
+#     4. vscode extension 설치(Vetur, Html css support, vue 3 Snippets)
+#     5. 만약 ERROR 뜨면 yarn을 설치하여 yarn으로 설치한다(안정적이고 빠름)
+         (yarn 최신버전을 install 하든지 또는 
+#         npm install --global yarn 이렇게도 yarn을 설치 가능하다)
+#     6. yarn global add @vue/cli 로 설치함.
+# 뷰라우터설치
+#     npm install vue-router --save
 # project 만들기
 #     npm create project명(폴더명)
 # 실행방법: npm run serve
 #    url 복사후 브라우저에 붙여넣기 하여 띄움
 # 강의:https://www.youtube.com/watch?v=-tVaahsXpwk(코딩애플)
 # 참조:https://www.youtube.com/watch?v=sqH0u8wN4Rs(한시간만에 끝내는 Vue.js 입문)
+#      bootstrap-vue.org 페이지 참조
 <template>
 <div>
   <div class="black-bg" v-if="modalstat==1">
